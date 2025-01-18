82a0b4365f0ce1c79ccee6fc4c5efd9cfb898f98
I love Frank Ocean <32484078+picharsiu@users.noreply.github.com>
2025-01-19 02:55:38 +0800

    Update zen.nix

--- home/software/browsers/zen.nix
+++ home/software/browsers/zen.nix
@@ -6,7 +6,7 @@
   # Create a wrapper script for zen-browser with Wayland enabled
   zenWithWayland = pkgs.symlinkJoin {
     name = "zen-browser-wayland";
-    paths = [inputs.zen-browser.packages."${pkgs.system}".stable];
+    paths = [inputs.zen-browser.packages."${pkgs.system}".release];
     buildInputs = [pkgs.makeWrapper];
     postBuild = ''
       wrapProgram $out/bin/zen \
